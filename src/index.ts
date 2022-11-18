35779300086e3603bb49b87d01be6c081a340a6e
Thiago S. Santos <ssthiago.dev@gmail.com>
2022-11-17 22:57:35 -0300

    Add create category route

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,13 @@
 import cors from 'cors';
 import express from 'express';
 
+import { categoriesRoutes } from './routes/categories.routes';
+
 export const app = express();
 app.use(express.json());
 app.use(cors());
+
+app.use(categoriesRoutes);
 
 app.get('/', (request, response) => {
   return response.json({ message: 'Hello World' });
