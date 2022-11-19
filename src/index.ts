a253d54bc1d374f8f4eceba0dcb1ddf998b88072
Thiago S. Santos <ssthiago.dev@gmail.com>
2022-11-19 15:14:24 -0300

    Refactor routes to routes/index

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
 import cors from 'cors';
 import express from 'express';
 
-import { categoriesRoutes } from './routes/categories.routes';
-import { specificationsRoutes } from './routes/specifications.routes';
+import { router } from './routes';
 
 export const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.use('/categories', categoriesRoutes);
-app.use('/specifications', specificationsRoutes);
+app.use(router);
 
-app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' });
+app.get('/health-check', (request, response) => {
+  return response.json({ result: 'healthy instance' });
 });
