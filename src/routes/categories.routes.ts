3ab210a41fc30bad74370e22c14198a75f0c3529
Thiago S. Santos <ssthiago.dev@gmail.com>
2022-11-26 18:59:56 -0300

    Update categories routes with functions

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express';
 import multer from 'multer';
 
-import { createCategoryController } from '../modules/cars/useCases/createCategory';
-import { importCategoriesController } from '../modules/cars/useCases/importCategories';
-import { listCategoriesController } from '../modules/cars/useCases/listCategories';
+import createCategoryController from '../modules/cars/useCases/createCategory';
+import importCategoriesController from '../modules/cars/useCases/importCategories';
+import listCategoriesController from '../modules/cars/useCases/listCategories';
 
 export const categoriesRoutes = Router();
 const upload = multer({
@@ -11,13 +11,13 @@ const upload = multer({
 });
 
 categoriesRoutes.post('/', (request, response) => {
-  return createCategoryController.handle(request, response);
+  return createCategoryController().handle(request, response);
 });
 
 categoriesRoutes.get('/', (request, response) => {
-  return listCategoriesController.handle(request, response);
+  return listCategoriesController().handle(request, response);
 });
 
 categoriesRoutes.post('/import', upload.single('file'), (request, response) => {
-  return importCategoriesController.handle(request, response);
+  return importCategoriesController().handle(request, response);
 });
